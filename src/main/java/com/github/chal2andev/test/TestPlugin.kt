6da3463b36c86cc07e1f919403e8635883fb035f
BuildTools <unconfigured@null.spigotmc.org>
2023-03-18 19:51:52 +0900

    kotlin plugin Sample setup

--- src/main/java/com/github/chal2andev/test/TestPlugin.kt
+++ src/main/java/com/github/chal2andev/test/TestPlugin.kt
@@ -19,11 +19,19 @@ class TestPlugin: JavaPlugin(), Listener{
 
     private fun registerKommand(){
         kommand {
+<<<<<<< HEAD
             register("앙기모찌"){
                 requires { isOp }
 
                 executes {
                     sender.sendMessage("서준 멍청이")
+=======
+            register("sample"){
+                requires { isOp }
+
+                executes {
+                    sender.sendMessage("kotlin, kommand plugin is enable")
+>>>>>>> f207352 (kotlin plugin Sample setup)
                 }
             }
         }
